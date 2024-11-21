2939e637a4c1188e7b2472df987347818cf2edd2
BelekKorchuev <belek01042005@gmail.com>
2024-11-21 19:29:44 +0600

    рабочая версия 1.0

--- detecting.py
+++ detecting.py
@@ -1,9 +1,8 @@
-from ctypes import c_wchar, c_char
-
 from bs4 import BeautifulSoup
 import hashlib
 import time
 from collections import deque
+from Main import logger
 
 # Допустимые типы сообщений
 valid_message_types = {
@@ -19,56 +18,59 @@ checked_messages = deque(maxlen=1000)
 
 def fetch_and_parse_first_page(driver):
     url = "https://old.bankrot.fedresurs.ru/Messages.aspx?attempt=1"
-    print(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] Открытие основной страницы...")
+    logger.info(f'[{time.strftime("%Y-%m-%d %H:%M:%S")}] Открытие основной страницы: {url}')
 
-    # Открываем страницу
-    driver.get(url)
-    time.sleep(1)  # Ждем 1 секунду для загрузки контента
+    try:
+        # Открываем страницу
+        driver.get(url)
+        time.sleep(1)  # Ждем 1 секунду для загрузки контента
 
-    # Получаем HTML-код страницы
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
+        # Получаем HTML-код страницы
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-    # Находим строки таблицы сообщений
-    table_rows = soup.select("table tr")
+        # Находим строки таблицы сообщений
+        table_rows = soup.select("table tr")
 
-    for row in table_rows:
-        cells = row.find_all("td")
-        if len(cells) < 5:
-            continue
+        for row in table_rows:
+            cells = row.find_all("td")
+            if len(cells) < 5:
+                continue
 
-        # Извлекаем данные из ячеек
-        date = cells[0].get_text(strip=True)
-        message_type = cells[1].get_text(strip=True)
-        debtor = cells[2].get_text(strip=True)
-        published_by = cells[4].get_text(strip=True)
-        link_messeges = cells[1].find("a")["href"] if cells[1].find("a") else None
-        link_arbitr = cells[4].find("a")["href"] if cells[4].find("a") else None
-        link_debtor = cells[2].find("a")["href"] if cells[2].find("a") else None
+            # Извлекаем данные из ячеек
+            date = cells[0].get_text(strip=True)
+            message_type = cells[1].get_text(strip=True)
+            debtor = cells[2].get_text(strip=True)
+            published_by = cells[4].get_text(strip=True)
+            link_messeges = cells[1].find("a")["href"] if cells[1].find("a") else None
+            link_arbitr = cells[4].find("a")["href"] if cells[4].find("a") else None
+            link_debtor = cells[2].find("a")["href"] if cells[2].find("a") else None
 
 
-        # Проверка типа сообщения
-        if message_type in valid_message_types:
-            # Создаем уникальный идентификатор для строки
-            msg_id = hashlib.md5((date + message_type + debtor).encode()).hexdigest()
+            # Проверка типа сообщения
+            if message_type in valid_message_types:
+                logger.info(f'Обнаружено релевантное сообщение типа: {message_type}')
+                # Создаем уникальный идентификатор для строки
+                msg_id = hashlib.md5((date + message_type + debtor).encode()).hexdigest()
 
-            new_messages = {
-                "дата": date,
-                "тип_сообщения": message_type,
-                "должник": debtor,
-                "должник_ссылка": f"https://old.bankrot.fedresurs.ru{link_debtor}" if link_debtor else "Нет ссылки",
-                "арбитр": published_by,
-                "арбитр_ссылка": f"https://old.bankrot.fedresurs.ru{link_arbitr}" if link_arbitr else "Нет ссылки",
-                "сообщение_ссылка": f"https://old.bankrot.fedresurs.ru{link_messeges}" if link_messeges else "Нет ссылки",
-            }
+                new_messages = {
+                    "дата": date,
+                    "тип_сообщения": message_type,
+                    "должник": debtor,
+                    "должник_ссылка": f"https://old.bankrot.fedresurs.ru{link_debtor}" if link_debtor else "Нет ссылки",
+                    "арбитр": published_by,
+                    "арбитр_ссылка": f"https://old.bankrot.fedresurs.ru{link_arbitr}" if link_arbitr else "Нет ссылки",
+                    "сообщение_ссылка": f"https://old.bankrot.fedresurs.ru{link_messeges}" if link_messeges else "Нет ссылки",
+                }
 
-            # Проверяем, является ли сообщение новым
-            if msg_id not in checked_messages:
-                checked_messages.append(msg_id)
+                # Проверяем, является ли сообщение новым
+                if msg_id not in checked_messages:
+                    checked_messages.append(msg_id)
+                    logger.info('Найдено новое релевантное сообщение')
+                    logger.debug(
+                        f'Дата: {date}, Тип сообщения: {message_type}, Должник: {debtor}, Кем опубликовано: {published_by}')
+                    return new_messages
 
-                print("\n\nНайдено релевантное сообщение:")
-                print(f"Дата: {date}")
-                print(f"Тип сообщения: {message_type}")
-                print(f"Должник: {debtor}")
-                print(f"Кем опубликовано: {published_by}")
-                print(f"Ссылка на сообщение: https://old.bankrot.fedresurs.ru{link_messeges}\n")
-                return new_messages
+    except Exception as e:
+        logger.error(f'Ошибка при обработке страницы {url}: {e}')
+
+    return None
